ac09aa1a0cac8a46fc63489c366442b23aa9f076
michaeljosephrosenthal <michael.the.awesome@gmail.com>
2015-06-22 14:57:32 +0800

    use commonjs2 export semantics

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,49 @@ var webpack = require("webpack");
 var path = require("path");
 
 module.exports = {
-	target:  "web",
-	cache:   false,
-	context: __dirname,
-	devtool: false,
-	entry:   ["./src/Static.jsx"],
-	output:  {
-		path:          path.join(__dirname, "dist"),
-		filename:      "static.js",
-		chunkFilename: "[name].[id].js"
-	},
-	module:  {
-		loaders: [
-			{include: /\.json$/, loaders: ["json-loader"]},
-			{
-                include: /\.js$|\.jsx$/,
-                loaders: [ "babel-loader?stage=0&optional=runtime&plugins=typecheck" ], 
-                exclude: /node_modules/
-            }
-		]
-	},
-	resolve: {
-		modulesDirectories: [
-			"src",
-			"node_modules",
-			"web_modules"
-		],
-		extensions: ["", ".json", ".js", ".jsx"]
-	},
-	node:    {
-		__dirname: true,
-		fs:        'empty'
-	}
+    target:  "node",
+    cache:   false,
+    context: __dirname,
+    devtool: false,
+    entry:   ["./src/Static.jsx"],
+    output:  {
+        library: "react-static-jsx",
+        libraryTarget: "commonjs2",
+        path:     path.join(__dirname, "dist"),
+        filename: "static.js"
+    },
+    externals: {
+        react: {
+            root: 'React',
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react'
+        }
+    },
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+    ],
+    module:  {
+        loaders: [
+            {
+            include: /\.js$|\.jsx$/,
+            loaders: [ "babel-loader?stage=0&optional=runtime&plugins=typecheck" ], 
+            exclude: /node_modules/
+        }
+        ]
+    },
+    resolve: {
+        modulesDirectories: [
+            "src",
+            "node_modules",
+            "web_modules"
+        ],
+        extensions: ["", ".json", ".js", ".jsx"]
+    },
+    node:    {
+        __dirname: true,
+        fs:        'empty'
+    }
 };
